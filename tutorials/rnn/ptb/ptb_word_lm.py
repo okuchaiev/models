6a8d9b18dc100f8ec26bfa4798be60d149cc038c
Oleksii Kuchaiev <okuchaiev@nvidia.com>
2017-01-18 15:54:08 -0800

    add sampling and param to pickle in PTB code

--- tutorials/rnn/ptb/ptb_word_lm.py
+++ tutorials/rnn/ptb/ptb_word_lm.py
@@ -14,11 +14,9 @@
 # ==============================================================================
 
 """Example / benchmark for building a PTB LSTM model.
-
 Trains the model described in:
 (Zaremba, et. al.) Recurrent Neural Network Regularization
 http://arxiv.org/abs/1409.2329
-
 There are 3 supported model configurations:
 ===========================================
 | config | epochs | train | valid  | test
@@ -27,7 +25,6 @@ There are 3 supported model configurations:
 | medium | 39     | 48.45 |  86.16 |  82.07
 | large  | 55     | 37.87 |  82.62 |  78.29
 The exact results may vary depending on the random initialization.
-
 The hyperparameters used in the model:
 - init_scale - the initial scale of the weights
 - learning_rate - the initial value of the learning rate
@@ -40,17 +37,12 @@ The hyperparameters used in the model:
 - keep_prob - the probability of keeping weights in the dropout layer
 - lr_decay - the decay of the learning rate for each epoch after "max_epoch"
 - batch_size - the batch size
-
 The data required for this example is in the data/ dir of the
 PTB dataset from Tomas Mikolov's webpage:
-
 $ wget http://www.fit.vutbr.cz/~imikolov/rnnlm/simple-examples.tgz
 $ tar xvf simple-examples.tgz
-
 To run:
-
 $ python ptb_word_lm.py --data_path=simple-examples/data/
-
 """
 from __future__ import absolute_import
 from __future__ import division
@@ -58,9 +50,10 @@ from __future__ import print_function
 
 import time
 import sys
-sys.stdout=sys.stderr
+sys.stdout = sys.stderr
 import numpy as np
 import tensorflow as tf
+import pickle
 import reader
 
 flags = tf.flags
@@ -75,6 +68,12 @@ flags.DEFINE_string("save_path", None,
                     "Model output directory.")
 flags.DEFINE_bool("use_fp16", False,
                   "Train using 16-bit floats instead of 32bit floats")
+flags.DEFINE_bool("sample_mode", False,
+                  "Must have trained model ready. Only does sampling")
+flags.DEFINE_string("file_prefix", "ptb",
+                  "will be looking for file_prefix.train.txt, file_prefix.test.txt and file_prefix.valid.txt in data_path")
+flags.DEFINE_string("seed_for_sample", "domestic sales of",
+                  "supply seeding phrase here. it must only contain words from vocabluary")
 
 FLAGS = flags.FLAGS
 
@@ -83,58 +82,48 @@ def data_type():
   return tf.float16 if FLAGS.use_fp16 else tf.float32
 
 
-class PTBInput(object):
-  """The input data."""
-
-  def __init__(self, config, data, name=None):
-    self.batch_size = batch_size = config.batch_size
-    self.num_steps = num_steps = config.num_steps
-    self.epoch_size = ((len(data) // batch_size) - 1) // num_steps
-    self.input_data, self.targets = reader.ptb_producer(
-        data, batch_size, num_steps, name=name)
-
-
 class PTBModel(object):
   """The PTB model."""
 
-  def __init__(self, is_training, config, input_):
-    self._input = input_
+  def __init__(self, is_training, config):
 
-    batch_size = input_.batch_size
-    num_steps = input_.num_steps
+    self.batch_size = batch_size = config.batch_size
+    self.num_steps = num_steps = config.num_steps
     size = config.hidden_size
     vocab_size = config.vocab_size
+
+    self._input_data = tf.placeholder(tf.int32, [batch_size, num_steps])
+    self._targets = tf.placeholder(tf.int32, [batch_size, num_steps])
 
     # Slightly better results can be obtained with forget gate biases
     # initialized to 1 but the hyperparameters of the model would need to be
     # different than reported in the paper.
-    lstm_cell = tf.contrib.rnn.BasicLSTMCell(
-        size, forget_bias=0.0, state_is_tuple=True)
+    lstm_cell = tf.contrib.rnn.BasicLSTMCell(size, forget_bias=0.0, state_is_tuple=True)
     if is_training and config.keep_prob < 1:
       lstm_cell = tf.contrib.rnn.DropoutWrapper(
           lstm_cell, output_keep_prob=config.keep_prob)
-    cell = tf.contrib.rnn.MultiRNNCell(
-        [lstm_cell] * config.num_layers, state_is_tuple=True)
+    cell = tf.contrib.rnn.MultiRNNCell([lstm_cell] * config.num_layers, state_is_tuple=True)
 
     self._initial_state = cell.zero_state(batch_size, data_type())
+
 
     with tf.device("/cpu:0"):
       embedding = tf.get_variable(
           "embedding", [vocab_size, size], dtype=data_type())
-      inputs = tf.nn.embedding_lookup(embedding, input_.input_data)
+      inputs = tf.nn.embedding_lookup(embedding, self._input_data)
 
     if is_training and config.keep_prob < 1:
       inputs = tf.nn.dropout(inputs, config.keep_prob)
 
-    # Simplified version of models/tutorials/rnn/rnn.py's rnn().
+    # Simplified version of tensorflow.models.rnn.rnn.py's rnn().
     # This builds an unrolled LSTM for tutorial purposes only.
     # In general, use the rnn() or state_saving_rnn() from rnn.py.
     #
     # The alternative version of the code below is:
     #
-    # inputs = tf.unstack(inputs, num=num_steps, axis=1)
-    # outputs, state = tf.nn.rnn(cell, inputs,
-    #                            initial_state=self._initial_state)
+    # inputs = [tf.squeeze(input_step, [1])
+    #           for input_step in tf.split(1, num_steps, inputs)]
+    # outputs, state = tf.nn.rnn(cell, inputs, initial_state=self._initial_state)
     outputs = []
     state = self._initial_state
     with tf.variable_scope("RNN"):
@@ -148,13 +137,14 @@ class PTBModel(object):
         "softmax_w", [size, vocab_size], dtype=data_type())
     softmax_b = tf.get_variable("softmax_b", [vocab_size], dtype=data_type())
     logits = tf.matmul(output, softmax_w) + softmax_b
+    self.sample = tf.multinomial(logits, 1)
+    #self.sample = tf.argmax(tf.nn.softmax(logits), 1)
     loss = tf.contrib.legacy_seq2seq.sequence_loss_by_example(
         [logits],
-        [tf.reshape(input_.targets, [-1])],
+        [tf.reshape(self._targets, [-1])],
         [tf.ones([batch_size * num_steps], dtype=data_type())])
     self._cost = cost = tf.reduce_sum(loss) / batch_size
     self._final_state = state
-
     if not is_training:
       return
 
@@ -162,7 +152,17 @@ class PTBModel(object):
     tvars = tf.trainable_variables()
     grads, _ = tf.clip_by_global_norm(tf.gradients(cost, tvars),
                                       config.max_grad_norm)
-    optimizer = tf.train.GradientDescentOptimizer(self._lr)
+    if config.optimizer == 'RMSPropOptimizer':
+      optimizer = tf.train.RMSPropOptimizer(self._lr)
+    elif config.optimizer == 'AdamOptimizer':
+      optimizer = tf.train.AdamOptimizer()
+    elif config.optimizer == 'AdagradOptimizer':
+      optimizer = tf.train.AdagradOptimizer(0.2)
+    elif config.optimizer == 'MomentumOptimizer':
+      optimizer = tf.train.MomentumOptimizer(self._lr, momentum=0.8, use_nesterov=True)
+    else:
+      optimizer = tf.train.GradientDescentOptimizer(self._lr)
+    #optimizer = tf.train.RMSPropOptimizer(self._lr)
     self._train_op = optimizer.apply_gradients(
         zip(grads, tvars),
         global_step=tf.contrib.framework.get_or_create_global_step())
@@ -175,8 +175,13 @@ class PTBModel(object):
     session.run(self._lr_update, feed_dict={self._new_lr: lr_value})
 
   @property
-  def input(self):
-    return self._input
+  def input_data(self):
+    return self._input_data
+
+  @property
+  def targets(self):
+    return self._targets
+
 
   @property
   def initial_state(self):
@@ -201,6 +206,8 @@ class PTBModel(object):
 
 class SmallConfig(object):
   """Small config."""
+  is_char_model = False
+  optimizer = 'AdamOptimizer'
   init_scale = 0.1
   learning_rate = 1.0
   max_grad_norm = 5
@@ -215,24 +222,10 @@ class SmallConfig(object):
   vocab_size = 10000
 
 
-class MediumConfig(object):
-  """Medium config."""
-  init_scale = 0.05
-  learning_rate = 1.0
-  max_grad_norm = 5
-  num_layers = 2
-  num_steps = 35
-  hidden_size = 650
-  max_epoch = 6
-  max_max_epoch = 39
-  keep_prob = 0.5
-  lr_decay = 0.8
-  batch_size = 20
-  vocab_size = 10000
-
-
 class LargeConfig(object):
   """Large config."""
+  is_char_model = False
+  optimizer = 'GradientDescentOptimizer'
   init_scale = 0.04
   learning_rate = 1.0
   max_grad_norm = 10
@@ -246,9 +239,47 @@ class LargeConfig(object):
   batch_size = 20
   vocab_size = 10000
 
+class CharLargeConfig(object):
+  """Large config."""
+  is_char_model = True
+  optimizer = 'AdagradOptimizer'
+  init_scale = 0.004
+  learning_rate = 0.05
+  max_grad_norm = 15
+  num_layers = 2
+  num_steps = 64
+  hidden_size = 512
+  max_epoch = 5
+  max_max_epoch = 5
+  keep_prob = 0.5
+  lr_decay = 1 / 1.15  
+  batch_size = 256
+  vocab_size = 10000
+
+
+class CharSmallConfig(object):
+  """Large config."""
+  is_char_model = True
+  optimizer = 'RMSPropOptimizer'
+  init_scale = 0.04
+  learning_rate = 0.05
+  max_grad_norm = 15
+  num_layers = 3
+  num_steps = 50
+  hidden_size = 256
+  max_epoch = 14
+  max_max_epoch = 155
+  keep_prob = 0.5
+  lr_decay = 1 / 1.15
+  batch_size = 8
+  vocab_size = 10000
+
+
 
 class TestConfig(object):
   """Tiny config, for testing."""
+  is_char_model = False
+  optimizer = 'GradientDescentOptimizer'
   init_scale = 0.1
   learning_rate = 1.0
   max_grad_norm = 1
@@ -263,39 +294,79 @@ class TestConfig(object):
   vocab_size = 10000
 
 
-def run_epoch(session, model, eval_op=None, verbose=False):
+def do_sample(session, model, data, num_samples):
+  """Sampled from the model"""
+  samples = []
+  state = session.run(model.initial_state)
+  fetches = [model.final_state, model.sample]
+  sample = None
+  for x in data:
+    feed_dict = {}
+    feed_dict[model.input_data] = [[x]]
+    for layer_num, (c, h) in enumerate(model.initial_state):
+      feed_dict[c] = state[layer_num].c
+      feed_dict[h] = state[layer_num].h
+
+    state, sample = session.run(fetches, feed_dict)
+  if sample is not None:
+    samples.append(sample[0][0])
+    #samples.append(sample[0])
+  else:
+    samples.append(0)
+  k = 1
+  while k < num_samples:
+    feed_dict = {}
+    feed_dict[model.input_data] = [[samples[-1]]]
+    for layer_num, (c, h) in enumerate(model.initial_state):
+      feed_dict[c] = state[layer_num].c
+      feed_dict[h] = state[layer_num].h
+    state, sample = session.run(fetches, feed_dict)
+    samples.append(sample[0][0])
+    #samples.append(sample[0])
+    k += 1
+  return samples
+
+
+def run_epoch(session, model, data, is_train=False, verbose=False):
   """Runs the model on the given data."""
+  epoch_size = ((len(data) // model.batch_size) - 1) // model.num_steps
   start_time = time.time()
   costs = 0.0
   iters = 0
   state = session.run(model.initial_state)
 
-  fetches = {
-      "cost": model.cost,
-      "final_state": model.final_state,
-  }
-  if eval_op is not None:
-    fetches["eval_op"] = eval_op
-
-  for step in range(model.input.epoch_size):
+  for step, (x, y) in enumerate(reader.ptb_iterator(data, model.batch_size, model.num_steps)):
+    if is_train:
+      fetches = [model.cost, model.final_state, model.train_op]
+    else:
+      fetches = [model.cost, model.final_state]
     feed_dict = {}
-    for i, (c, h) in enumerate(model.initial_state):
-      feed_dict[c] = state[i].c
-      feed_dict[h] = state[i].h
+    feed_dict[model.input_data] = x
+    feed_dict[model.targets] = y
+    for layer_num, (c, h) in enumerate(model.initial_state):
+      feed_dict[c] = state[layer_num].c
+      feed_dict[h] = state[layer_num].h
 
-    vals = session.run(fetches, feed_dict)
-    cost = vals["cost"]
-    state = vals["final_state"]
+    if is_train:
+      cost, state, _ = session.run(fetches, feed_dict)
+    else:
+      cost, state = session.run(fetches, feed_dict)
 
     costs += cost
-    iters += model.input.num_steps
+    iters += model.num_steps
 
-    if verbose and step % (model.input.epoch_size // 10) == 10:
+    if verbose and step % (epoch_size // 10) == 10:
       print("%.3f perplexity: %.3f speed: %.0f wps" %
-            (step * 1.0 / model.input.epoch_size, np.exp(costs / iters),
-             iters * model.input.batch_size / (time.time() - start_time)))
+            (step * 1.0 / epoch_size, np.exp(costs / iters),
+             iters * model.batch_size / (time.time() - start_time)))
 
   return np.exp(costs / iters)
+
+def pretty_print(items, is_char_model, id2word):
+  if not is_char_model:
+    return ' '.join([id2word[x] for x in items])
+  else:
+    return ''.join([id2word[x] for x in items]).replace('_', ' ')
 
 
 def get_config():
@@ -307,64 +378,108 @@ def get_config():
     return LargeConfig()
   elif FLAGS.model == "test":
     return TestConfig()
+  elif FLAGS.model == "charlarge":
+    return CharLargeConfig()
+  elif FLAGS.model == "charlarge1":
+    return CharLargeConfig1()
+  elif FLAGS.model == "charsmall":
+    return CharSmallConfig()
   else:
     raise ValueError("Invalid model: %s", FLAGS.model)
-
 
 def main(_):
   if not FLAGS.data_path:
     raise ValueError("Must set --data_path to PTB data directory")
 
-  raw_data = reader.ptb_raw_data(FLAGS.data_path)
-  train_data, valid_data, test_data, _ = raw_data
-
+  raw_data = reader.ptb_raw_data(FLAGS.data_path, FLAGS.file_prefix)
+  train_data, valid_data, test_data, word_to_id, id_2_word = raw_data
+  vocab_size = len(word_to_id)
+  #print(word_to_id)
+  print('Distinct terms: %d' % vocab_size)
   config = get_config()
+  config.vocab_size = config.vocab_size if config.vocab_size < vocab_size else vocab_size
   eval_config = get_config()
+  eval_config.vocab_size = eval_config.vocab_size if eval_config.vocab_size < vocab_size else vocab_size
   eval_config.batch_size = 1
   eval_config.num_steps = 1
+
+  if config.is_char_model:
+    seed_for_sample = [c for c in FLAGS.seed_for_sample.replace('\'','').replace(' ', '_')]
+  else:
+    seed_for_sample = FLAGS.seed_for_sample.split()
 
   with tf.Graph().as_default():
     initializer = tf.random_uniform_initializer(-config.init_scale,
                                                 config.init_scale)
-
     with tf.name_scope("Train"):
-      train_input = PTBInput(config=config, data=train_data, name="TrainInput")
+
       with tf.variable_scope("Model", reuse=None, initializer=initializer):
-        m = PTBModel(is_training=True, config=config, input_=train_input)
-      tf.scalar_summary("Training Loss", m.cost)
-      tf.scalar_summary("Learning Rate", m.lr)
+        m = PTBModel(is_training=True, config=config)
+        tf.summary.scalar("Training Loss", m.cost)
+        tf.summary.scalar("Learning Rate", m.lr)
 
     with tf.name_scope("Valid"):
-      valid_input = PTBInput(config=config, data=valid_data, name="ValidInput")
       with tf.variable_scope("Model", reuse=True, initializer=initializer):
-        mvalid = PTBModel(is_training=False, config=config, input_=valid_input)
-      tf.scalar_summary("Validation Loss", mvalid.cost)
+        mvalid = PTBModel(is_training=False, config=config)
+        tf.summary.scalar("Validation Loss", mvalid.cost)
 
     with tf.name_scope("Test"):
-      test_input = PTBInput(config=eval_config, data=test_data, name="TestInput")
       with tf.variable_scope("Model", reuse=True, initializer=initializer):
-        mtest = PTBModel(is_training=False, config=eval_config,
-                         input_=test_input)
+        mtest = PTBModel(is_training=False, config=eval_config)
 
-    sv = tf.train.Supervisor(logdir=FLAGS.save_path)
+    saver = tf.train.Saver(name='saver', write_version=tf.train.SaverDef.V2)
+    sv = tf.train.Supervisor(logdir=FLAGS.save_path, save_model_secs=0, save_summaries_secs=0, saver=saver)
+
+    old_valid_perplexity = 10000000000.0
     with sv.managed_session() as session:
+      if FLAGS.sample_mode:
+        while True:
+          inpt = raw_input("Enter your sample prefix: ")
+          cnt = int(raw_input("Sample size: "))
+          if config.is_char_model:
+            seed_for_sample = [c for c in inpt.replace(' ', '_')]
+          else:
+            seed_for_sample = inpt.split()
+          print("Seed: %s" % pretty_print([word_to_id[x] for x in seed_for_sample], config.is_char_model, id_2_word))
+          print("Sample: %s" % pretty_print(do_sample(session, mtest, [word_to_id[word] for word in seed_for_sample],
+                                                      cnt), config.is_char_model, id_2_word))
+
       for i in range(config.max_max_epoch):
-        lr_decay = config.lr_decay ** max(i + 1 - config.max_epoch, 0.0)
+
+        print("Seed: %s" % pretty_print([word_to_id[x] for x in seed_for_sample], config.is_char_model, id_2_word))
+        print("Sample: %s" % pretty_print(do_sample(session, mtest, [word_to_id[word] for word in seed_for_sample],
+                                                    max(5 * (len(seed_for_sample) + 1), 10)), config.is_char_model, id_2_word))
+
+        lr_decay = config.lr_decay ** max(i - config.max_epoch, 0.0)
         m.assign_lr(session, config.learning_rate * lr_decay)
-
         print("Epoch: %d Learning rate: %.3f" % (i + 1, session.run(m.lr)))
-        train_perplexity = run_epoch(session, m, eval_op=m.train_op,
-                                     verbose=True)
+        train_perplexity = run_epoch(session, m, train_data, is_train=True, verbose=True)
         print("Epoch: %d Train Perplexity: %.3f" % (i + 1, train_perplexity))
-        valid_perplexity = run_epoch(session, mvalid)
+        valid_perplexity = run_epoch(session, mvalid, valid_data)
         print("Epoch: %d Valid Perplexity: %.3f" % (i + 1, valid_perplexity))
+        if valid_perplexity < old_valid_perplexity:
+          old_valid_perplexity = valid_perplexity
+          sv.saver.save(session, FLAGS.save_path, i)
+        elif valid_perplexity >= 1.3*old_valid_perplexity:
+          if len(sv.saver.last_checkpoints)>0:
+            sv.saver.restore(session, sv.saver.last_checkpoints[-1])
+          break
+        else:
+          if len(sv.saver.last_checkpoints)>0:
+            sv.saver.restore(session, sv.saver.last_checkpoints[-1])
+          lr_decay *=0.5
 
-      test_perplexity = run_epoch(session, mtest)
+      print("Computing Test set perplexity")
+      test_perplexity = run_epoch(session, mtest, test_data)
       print("Test Perplexity: %.3f" % test_perplexity)
-
-      if FLAGS.save_path:
-        print("Saving model to %s." % FLAGS.save_path)
-        sv.saver.save(session, FLAGS.save_path, global_step=sv.global_step)
+      print('ALL TRAINABLE VARIABLES')
+      params = {}
+      variables = tf.trainable_variables()
+      for v in variables:
+        print(v.name)
+        params[v.name]=v.eval(session=session)       
+      print('END OF ALL TRAINABLE VARIABLED')
+      pickle.dump(params, open( "params.p", "wb" ) )
 
 
 if __name__ == "__main__":
