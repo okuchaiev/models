3ccc7599561d7ec3177f5ef25e95461afaeeb899
Oleksii Kuchaiev <okuchaiev@nvidia.com>
2017-02-13 13:33:48 -0800

    change gradient accumulation mode in seq2seq

--- tutorials/rnn/translate/seq2seq_model.py
+++ tutorials/rnn/translate/seq2seq_model.py
@@ -206,7 +206,7 @@ class Seq2SeqModel(object):
       self.updates = []
       opt = tf.train.GradientDescentOptimizer(self.learning_rate)
       for b in xrange(len(buckets)):
-        gradients = tf.gradients(self.losses[b], params)
+        gradients = tf.gradients(self.losses[b], params, aggregation_method=tf.AggregationMethod.EXPERIMENTAL_ACCUMULATE_N)
         clipped_gradients, norm = tf.clip_by_global_norm(gradients,
                                                          max_gradient_norm)
         self.c_grads = clipped_gradients
